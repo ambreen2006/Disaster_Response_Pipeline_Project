55e1dd206a71fd895128547c79b56ad23c2eda44
Ambreen System76 <ambreen2006@gmail.com>
2020-06-05 10:00:37 -0400

    feat: Implementation of the ETL Pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,17 +1,45 @@
 import sys
+import numpy as np
+import pandas as pd
 
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on = 'id')
+    return df
 
 
 def clean_data(df):
-    pass
+
+    def get_categories_as_dataframe(data):
+        cats = data.categories.str.split(';', expand = True)
+        row = cats.iloc[0,:]
+        category_colnames = row.apply(lambda x: x[:-2])
+        cats.columns = category_colnames
+        return cats
+
+    def clean_categories(data):
+        cats = get_categories_as_dataframe(data)
+        for column in cats:
+            cats[column]  = cats[column].str[-1]
+            cats[column] = cats[column].astype('int')
+        data = pd.concat([data.drop(columns = ['categories']), cats],
+                        axis = 1)
+        return data
+
+    def remove_duplicates(data):
+        data = data.drop_duplicates()
+        return data
+
+    return remove_duplicates(clean_categories(df))
 
 
 def save_data(df, database_filename):
-    pass  
-
+    uri = 'sqlite:///' + database_filename
+    engine = create_engine(uri)
+    df.to_sql('Messages', engine, index = False)
 
 def main():
     if len(sys.argv) == 4:
@@ -24,12 +52,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
@@ -37,7 +65,6 @@ def main():
               'to as the third argument. \n\nExample: python process_data.py '\
               'disaster_messages.csv disaster_categories.csv '\
               'DisasterResponse.db')
-
 
 if __name__ == '__main__':
     main()
