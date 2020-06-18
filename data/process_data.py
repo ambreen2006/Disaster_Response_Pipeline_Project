214c6cc8128653b572f17279523f840d77f65f0a
Ambreen System76 <ambreen2006@gmail.com>
2020-06-17 22:41:44 -0400

    fix: Replace database if already exist

--- data/process_data.py
+++ data/process_data.py
@@ -46,7 +46,7 @@ def save_data(df, database_filename):
     '''
     uri = 'sqlite:///' + database_filename
     engine = create_engine(uri)
-    df.to_sql('Messages', engine, index = False)
+    df.to_sql('Messages', engine, index = False, if_exists='replace')
 
 def main():
     if len(sys.argv) == 4:
